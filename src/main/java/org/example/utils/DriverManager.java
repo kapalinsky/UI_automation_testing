84f20fdc60f0b797674f5452b2287af816aa4963
Artyom Kapalinsky <89062428+kapalinsky@users.noreply.github.com>
2023-04-05 13:35:51 +0300

    [master]: add ChromeDriver Options

--- src/main/java/org/example/utils/DriverManager.java
+++ src/main/java/org/example/utils/DriverManager.java
@@ -3,6 +3,7 @@ package org.example.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
